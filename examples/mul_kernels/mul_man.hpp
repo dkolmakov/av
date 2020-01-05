2bfaabf6ea4e7240a9727b2679fed17fa51af775
Dmitry Kolmakov <ds.kolmakov@gmail.com>
2020-01-05 12:37:01 +0300

    Generate code to use one-value chunks in manual multiplication algorithm

--- examples/mul_kernels/mul_man.hpp
+++ examples/mul_kernels/mul_man.hpp
@@ -9,77 +9,83 @@
 
 namespace mul_man {
     
-namespace implementation {
+namespace impl {
 
 #ifdef __AVX512F__
-    constexpr std::size_t NUMBERS_PER_OP = 8;
+    constexpr std::size_t VALS_PER_OP = 8;
 #elif __AVX__
-    constexpr std::size_t NUMBERS_PER_OP = 4;
+    constexpr std::size_t VALS_PER_OP = 4;
 #elif __SSE4_1__
-    constexpr std::size_t NUMBERS_PER_OP = 2;
+    constexpr std::size_t VALS_PER_OP = 2;
 #else
-    constexpr std::size_t NUMBERS_PER_OP = 1;
+    constexpr std::size_t VALS_PER_OP = 1;
 #endif
     
-    template <class T, std::size_t chunk_size, std::size_t parity_checker = chunk_size % NUMBERS_PER_OP, std::size_t reg_size = av::SIMD_REG_SIZE>
+    template <class T, std::size_t chunk_size, std::size_t step, std::size_t parity_checker = (chunk_size * step) % VALS_PER_OP, std::size_t reg_size = av::SIMD_REG_SIZE>
     struct chunk_mul;
-    
-    template <class T, std::size_t chunk_size>
-    struct chunk_mul<T, chunk_size, 0, 32> {
-        static force_inline void compute(std::complex<T> *acc, std::complex<T> *arr) {
-            __m256d realA[chunk_size / 4];
-            __m256d imagA[chunk_size / 4]; 
-            avx::unpack_complex<T, chunk_size / 4 - 1>::unpack(realA, imagA, acc);
-            
-            __m256d realB[chunk_size / 4];
-            __m256d imagB[chunk_size / 4];
-            avx::unpack_complex<T, chunk_size / 4 - 1>::unpack(realB, imagB, arr);
 
-            avx::multiply<T, chunk_size / 4 - 1>::compute(realA, imagA, realB, imagB);
-            avx::pack<T, chunk_size / 4 - 1>::make(realA, imagA, acc);
+    template <class T, std::size_t chunk_size, std::size_t step>
+    struct chunk_mul<T, chunk_size, step, 0, 32> {
+        constexpr static std::size_t portion_size = chunk_size * step;
+        
+        static force_inline void compute(std::complex<T> **acc, std::complex<T> **arr) {
+            __m256d realA[portion_size / 4];
+            __m256d imagA[portion_size / 4]; 
+            avx::unpack<T, portion_size / 4 - 1, step>::doIt(realA, imagA, acc);
+            
+            __m256d realB[portion_size / 4];
+            __m256d imagB[portion_size / 4];
+            avx::unpack<T, portion_size / 4 - 1, step>::doIt(realB, imagB, arr);
+
+            avx::multiply<T, portion_size / 4 - 1>::compute(realA, imagA, realB, imagB);
+            avx::pack<T, portion_size / 4 - 1, step>::doIt(realA, imagA, acc);
         }
     };
 
-    template <class T, std::size_t chunk_size>
-    struct chunk_mul<T, chunk_size, 0, 16> {
-        static force_inline void compute(std::complex<T> *acc, std::complex<T> *arr) {
-            __m128d realA[chunk_size / 2];
-            __m128d imagA[chunk_size / 2]; 
-            sse::unpack_complex<T, chunk_size / 2 - 1>::unpack(realA, imagA, acc);
+    template <class T, std::size_t chunk_size, std::size_t step>
+    struct chunk_mul<T, chunk_size, step, 0, 16> {
+        constexpr static std::size_t portion_size = chunk_size * step;
+        
+        static force_inline void compute(std::complex<T> **acc, std::complex<T> **arr) {
+            __m128d realA[portion_size / 2];
+            __m128d imagA[portion_size / 2]; 
+            sse::unpack<T, portion_size / 2 - 1, step>::doIt(realA, imagA, acc);
             
-            __m128d realB[chunk_size / 2];
-            __m128d imagB[chunk_size / 2];
-            sse::unpack_complex<T, chunk_size / 2 - 1>::unpack(realB, imagB, arr);
+            __m128d realB[portion_size / 2];
+            __m128d imagB[portion_size / 2];
+            sse::unpack<T, portion_size / 2 - 1, step>::doIt(realB, imagB, arr);
 
-            sse::multiply<T, chunk_size / 2 - 1>::compute(realA, imagA, realB, imagB);
-            sse::pack<T, chunk_size / 2 - 1>::make(realA, imagA, acc);
+            sse::multiply<T, portion_size / 2 - 1>::compute(realA, imagA, realB, imagB);
+            sse::pack<T, portion_size / 2 - 1, step>::doIt(realA, imagA, acc);
         }
     };
     
-    template <class T, std::size_t chunk_size, std::size_t parity_checker, std::size_t reg_size>
+    template <class T, std::size_t chunk_size, std::size_t step, std::size_t parity_checker, std::size_t reg_size>
     struct chunk_mul {
-        static force_inline void compute(std::complex<T> *acc, std::complex<T> *arr) {
-            for (std::size_t i = 0; i < chunk_size; i++)
-                acc[i] *= arr[i];
+        static force_inline void compute(std::complex<T> **acc, std::complex<T> **arr) {
+            for (std::size_t i = 0; i < step; i++) {
+                for (std::size_t j = 0; j < chunk_size; j++)
+                    acc[i][j] *= arr[i][j];
+            }
         }
     };
     
     
-    template <class T, std::size_t chunk_size, std::size_t index>
+    template <class T, std::size_t chunk_size, std::size_t step, std::size_t index>
     struct unroll_chunks;
 
-    template <class T, std::size_t chunk_size>
-    struct unroll_chunks<T, chunk_size, 0> {
+    template <class T, std::size_t chunk_size, std::size_t step>
+    struct unroll_chunks<T, chunk_size, step, 0> {
         static force_inline void compute(std::complex<T> **left, std::complex<T> **right) {
-            chunk_mul<T, chunk_size>::compute(left[0], right[0]);
+            chunk_mul<T, chunk_size, step>::compute(&left[0], &right[0]);
         }
     };
     
-    template <class T, std::size_t chunk_size, std::size_t index>
+    template <class T, std::size_t chunk_size, std::size_t step, std::size_t index>
     struct unroll_chunks {
         static force_inline void compute(std::complex<T> **left, std::complex<T> **right) {
-            unroll_chunks<T, chunk_size, index - 1>::compute(left, right);
-            chunk_mul<T, chunk_size>::compute(left[index], right[index]);
+            unroll_chunks<T, chunk_size, step, index - step>::compute(left, right);
+            chunk_mul<T, chunk_size, step>::compute(&left[index], &right[index]);
         }
     };
     
@@ -92,8 +98,10 @@ namespace implementation {
         
         template <class T, std::size_t chunk_size, std::size_t n_chunks>
         struct core {
+            constexpr static std::size_t step = (chunk_size < impl::VALS_PER_OP && (impl::VALS_PER_OP % chunk_size == 0) && (n_chunks % (impl::VALS_PER_OP / chunk_size)  == 0)) ? impl::VALS_PER_OP / chunk_size : 1;
+
             static force_inline void compute(std::complex<T> **left, std::complex<T> **right) {
-                implementation::unroll_chunks<T, chunk_size, n_chunks - 1>::compute(left, right);
+                impl::unroll_chunks<T, chunk_size, step, n_chunks - step>::compute(left, right);
             }
         };
     };
--- examples/mul_kernels/mul_sse.hpp
+++ examples/mul_kernels/mul_sse.hpp
@@ -9,27 +9,76 @@ namespace mul_man {
     
 namespace sse {
 
-    template <class T, std::size_t index>
-    struct unpack_complex;
+    template <class T, std::size_t index, std::size_t step>
+    struct unpack;
 
     template <class T>
-    struct unpack_complex<T, 0> {
-        static force_inline void unpack(__m128d *realB, __m128d *imagB, std::complex<T> *arr) {
-            __m128d B0 = _mm_loadu_pd((double*)(arr + 0 * 2));
-            __m128d B2 = _mm_loadu_pd((double*)(arr + 0 * 2 + 1));
+    struct unpack<T, 0, 1> {
+        static force_inline void doIt(__m128d *realB, __m128d *imagB, std::complex<T> **arr) {
+            __m128d B0 = _mm_loadu_pd((double*)(arr[0] + 0 * 2));
+            __m128d B2 = _mm_loadu_pd((double*)(arr[0] + 0 * 2 + 1));
             realB[0] = _mm_unpacklo_pd(B0, B2);
             imagB[0] = _mm_unpackhi_pd(B0, B2);
         }
     };
 
     template <class T, std::size_t index>
-    struct unpack_complex {
-        static force_inline void unpack(__m128d *realB, __m128d *imagB, std::complex<T> *arr) {
-            __m128d B0 = _mm_loadu_pd((double*)(arr + index * 2));
-            __m128d B2 = _mm_loadu_pd((double*)(arr + index * 2 + 1));
+    struct unpack<T, index, 1> {
+        static force_inline void doIt(__m128d *realB, __m128d *imagB, std::complex<T> **arr) {
+            __m128d B0 = _mm_loadu_pd((double*)(arr[0] + index * 2));
+            __m128d B2 = _mm_loadu_pd((double*)(arr[0] + index * 2 + 1));
             realB[index] = _mm_unpacklo_pd(B0, B2);
             imagB[index] = _mm_unpackhi_pd(B0, B2);
-            unpack_complex<T, index - 1>::unpack(realB, imagB, arr);
+            unpack<T, index - 1, 1>::doIt(realB, imagB, arr);
+        }
+    };
+    
+    template <class T>
+    struct unpack<T, 0, 2> {
+        static force_inline void doIt(__m128d *realB, __m128d *imagB, std::complex<T> **arr) {
+            double *to_load0 = (double*)arr[0];
+            double *to_load1 = (double*)arr[1];
+            realB[0] = _mm_set_pd(to_load0[0], to_load1[0]);
+            imagB[0] = _mm_set_pd(to_load0[1], to_load1[1]);
+        }
+    };
+    
+    template <class T, std::size_t index, std::size_t step>
+    struct pack;
+
+    template <class T>
+    struct pack<T, 0, 1> {
+        static force_inline void doIt(__m128d *realA, __m128d *imagA, std::complex<T> **acc) {
+            __m128d dst0 = _mm_shuffle_pd(realA[0], imagA[0], 0b00);
+            __m128d dst2 = _mm_shuffle_pd(realA[0], imagA[0], 0b11);
+            _mm_storeu_pd((double*)(acc[0] + 0 * 2), dst0);
+            _mm_storeu_pd((double*)(acc[0] + 0 * 2 + 1), dst2);
+        }
+    };
+
+    template <class T, std::size_t index>
+    struct pack<T, index, 1> {
+        static force_inline void doIt(__m128d *realA, __m128d *imagA, std::complex<T> **acc) {
+            __m128d dst0 = _mm_shuffle_pd(realA[index], imagA[index], 0b00);
+            __m128d dst2 = _mm_shuffle_pd(realA[index], imagA[index], 0b11);
+            _mm_storeu_pd((double*)(acc[0] + index * 2), dst0);
+            _mm_storeu_pd((double*)(acc[0] + index * 2 + 1), dst2);
+
+            pack<T, index - 1, 1>::doIt(realA, imagA, acc);
+        }
+    };
+
+    template <class T>
+    struct pack<T, 0, 2> {
+        static force_inline void doIt(__m128d *realA, __m128d *imagA, std::complex<T> **acc) {
+            double *to_store0 = (double *)acc[0];
+            double *to_store1 = (double *)acc[1];
+            double *real = (double *)realA;
+            double *imag = (double *)imagA;
+            to_store0[0] = real[0];
+            to_store0[1] = imag[0];
+            to_store1[0] = real[1];
+            to_store1[1] = imag[1];
         }
     };
     
@@ -66,31 +115,6 @@ namespace sse {
         }
     };
 
-    template <class T, std::size_t index>
-    struct pack;
-
-    template <class T>
-    struct pack<T, 0> {
-        static force_inline void make(__m128d *realA, __m128d *imagA, std::complex<T> *acc) {
-            __m128d dst0 = _mm_shuffle_pd(realA[0], imagA[0], 0b00);
-            __m128d dst2 = _mm_shuffle_pd(realA[0], imagA[0], 0b11);
-            _mm_storeu_pd((double*)(acc + 0 * 2), dst0);
-            _mm_storeu_pd((double*)(acc + 0 * 2 + 1), dst2);
-        }
-    };
-
-    template <class T, std::size_t index>
-    struct pack {
-        static force_inline void make(__m128d *realA, __m128d *imagA, std::complex<T> *acc) {
-            __m128d dst0 = _mm_shuffle_pd(realA[index], imagA[index], 0b00);
-            __m128d dst2 = _mm_shuffle_pd(realA[index], imagA[index], 0b11);
-            _mm_storeu_pd((double*)(acc + index * 2), dst0);
-            _mm_storeu_pd((double*)(acc + index * 2 + 1), dst2);
-
-            pack<T, index - 1>::make(realA, imagA, acc);
-        }
-    };
-    
 }
 
 
